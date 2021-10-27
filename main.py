e7e411fb834c9afd05e34dd700da0459a71b5b00
Tassadaritze <Tassadaritze@gmail.com>
2021-10-27 21:13:04 +0300

    Release 1.0.0 (#3)
    Achieved basic functionality: the bot will farm expert maps with bonus rewards indefinitely (mostly)

--- main.py
+++ main.py
@@ -1,52 +1,159 @@
-import numpy as np
-import cv2 as cv
-import matplotlib.pyplot as plt
-from mss import mss
-import win32api as wa
+import random
+import sys
+import os
 from time import sleep
-import random as random
+
+import cv2 as cv
+import keyboard
+import win32api as wa
+
+import solutions
+import utils
+
+CONST_PLAY_BTN_LOC = [800, 950]
+CONST_EXPERT_BTN_LOC = [1300, 1000]
+CONST_NEXT_BTN_LOC = [960, 910]
+CONST_HOME_BTN_LOC = [700, 850]
+CONST_CHEST_BTN_LOC = [965, 395]
+CONST_INSTA_DUO_1_BTN_LOC = [810, 550]
+CONST_INSTA_DUO_2_BTN_LOC = [1110, 550]
+CONST_INSTA_TRIO_1_BTN_LOC = [656, 535]
+CONST_INSTA_TRIO_2_BTN_LOC = [956, 535]
+CONST_INSTA_TRIO_3_BTN_LOC = [1256, 535]
+CONST_CONTINUE_BTN_LOC = [950, 1000]
+CONST_CANCEL_BTN_LOC = [780, 730]
+CONST_NUMBER_OF_EXPERT_MAP_SCREENS = 2
+CONST_BONUS_TEMPLATE = cv.imread("bonus/pumpkin.png", cv.IMREAD_GRAYSCALE)  # thing to find
 
 
-def rand_click():
-    wa.mouse_event(2, 0, 0)
-    slp = random.randrange(97, 205)
-    print(slp)
-    sleep(slp / 1000)
-    wa.mouse_event(4, 0, 0)
+def get_map(page, x, y):
+    index = 0
+    expert_maps = ["sanctuary", "ravine", "flooded_valley", "infernal", "bloody_puddles", \
+                    "workshop", "quad", "dark_castle", "muddy_puddles", "ouch"]
+    top_row = y >= 120 and y <= 410
+    bot_row = y >= 434 and y <= 723
+    first_col = x >= 355 and x <= 717
+    second_col = x >= 779 and x <= 1141
+    third_col = x >= 1203 and x <= 1565
+
+    if second_col:
+        index += 1
+    elif third_col:
+        index += 2
+    if bot_row:
+        index += 3
+    while page > 0:
+        index += 6
+        page -= 1
+
+    print("Loading solution for " + expert_maps[index])
+
+    return expert_maps[index]
 
 
-def nav_to_expert():
-    wa.SetCursorPos([800, 950])
-    rand_click()
+# navigates from main menu to one of the expert map screens
+def nav_main_to_expert():
+    wa.SetCursorPos(CONST_PLAY_BTN_LOC)
+    utils.click()
     slp = random.randrange(904, 1473)
-    print(slp)
     sleep(slp / 1000)
-    wa.SetCursorPos([1300, 1000])
-    rand_click()
+    wa.SetCursorPos(CONST_EXPERT_BTN_LOC)
+    utils.click()
+    sleep(0.3)
 
 
-print("The program will take single screenshots of your first monitor for navigation purposes\n")
-nav_to_expert()
-# input("Open BTD6 main menu on monitor 1, then press any key to continue")
+def nav_victory_to_main():
+    wa.SetCursorPos(CONST_NEXT_BTN_LOC)
+    utils.click()
+    slp = random.randrange(904, 1473)
+    sleep(slp / 1000)
+    wa.SetCursorPos(CONST_HOME_BTN_LOC)
+    utils.click()
+    sleep(5)
 
-with mss() as sct:
-    sct.shot()
 
-""" template = cv.imread("bonus.png", 0)  # thing to find
-img_rgb = cv.imread("fake.png", 0)   # thing to search through
-img_gray = cv.imread("fake.png", cv.IMREAD_GRAYSCALE)
-w, h = template.shape[::-1]
+def reveal_insta(insta_position):
+    wa.SetCursorPos(insta_position)
+    utils.click()
+    sleep(0.3)
+    utils.click()
+    sleep(0.3)
 
-res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where(res >= threshold)  # tuple of matching coordinates
-print(loc)
-print(len(loc[0]))
-for pt in zip(*loc[::-1]):
-    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-cv.imwrite('res.png', img_rgb)
-plt.imshow(img_rgb), plt.show() """
+def open_chest():
+    wa.SetCursorPos(CONST_CHEST_BTN_LOC)
+    utils.click()
+    sleep(0.3)
+    reveal_insta(CONST_INSTA_TRIO_1_BTN_LOC)
+    reveal_insta(CONST_INSTA_DUO_1_BTN_LOC)
+    reveal_insta(CONST_INSTA_TRIO_2_BTN_LOC)
+    reveal_insta(CONST_INSTA_DUO_2_BTN_LOC)
+    reveal_insta(CONST_INSTA_TRIO_3_BTN_LOC)
+    wa.SetCursorPos(CONST_CONTINUE_BTN_LOC)
+    utils.click()
+    sleep(0.3)
+    utils.click()
+    sleep(0.3)
+    utils.press('escape')
+    sleep(0.3)
+    wa.SetCursorPos(CONST_CANCEL_BTN_LOC)
+    utils.click()
+    sleep(0.3)
 
-scrn = cv.imread("monitor-1.png", cv.IMREAD_GRAYSCALE)
-plt.imshow(scrn), plt.show()
+
+# returns the top left coordinates of tmpl inside of img
+def match_template(img, tmpl):
+    res = cv.matchTemplate(img, tmpl, cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    if max_val > 0.3:
+        return max_loc
+    else:
+        return False
+
+
+def main():
+    print("The program will take and store single screenshots of your first monitor for navigation purposes\n")
+
+    print("Navigate to Bloons TD 6 main menu on monitor 1, then press Enter to continue")
+    # print("Press Alt+1 to exit the program (hopefully)")
+    # keyboard.add_hotkey("alt+1", lambda: sys.exit(0), suppress=True)
+
+    # press enter after opening bloons on the main menu
+    keyboard.wait("enter")
+    
+    screenshot_path = "screenshots"
+    if not os.path.exists(screenshot_path):
+        os.makedirs(screenshot_path)
+
+    while True:
+        nav_main_to_expert()
+
+        match = False
+        page = 0
+
+        while not match:
+            sleep(0.3)
+            utils.take_screenshot()
+            # read in screenshot from file in grayscale
+            menu_screenshot = cv.imread("screenshots/monitor1.png", cv.IMREAD_GRAYSCALE)
+            match = match_template(menu_screenshot, CONST_BONUS_TEMPLATE)
+            if not match:
+                utils.click()
+                page = (page + 1) % CONST_NUMBER_OF_EXPERT_MAP_SCREENS
+        else:
+            utils.move_cursor(*match)
+            utils.click()
+            sleep(0.3)
+            utils.move_cursor(632, 582)
+            utils.click()
+            sleep(0.3)
+            utils.click()
+            sleep(8)
+            solutions.start_game()
+            solutions.solve(get_map(page, *match))
+            nav_victory_to_main()
+            open_chest()
+    return
+
+
+main()
---
+++ solutions.py
@@ -0,0 +1,599 @@
+import logging as log
+from time import sleep
+
+import cv2 as cv
+import numpy as np
+
+import utils
+from config import hotkeys
+
+CONST_UPG_TOP = "upg_top"
+CONST_UPG_MID = "upg_mid"
+CONST_UPG_BOT = "upg_bot"
+
+placed_monkeys = {}
+
+
+class Monkey:
+    def __init__(self, type, x, y):
+        self.type = type
+        self.x = x
+        self.y = y
+        self.upgrades = [0, 0, 0]
+        self.target = 0
+        self.targets = ["first", "last", "close", "strong"]
+        self.place()
+
+
+    def place(self):
+        utils.move_cursor(self.x, self.y)
+        desired_key = hotkeys[self.type]
+        utils.press(desired_key)
+        utils.click()
+    
+
+    def select(self):
+        utils.move_cursor(self.x, self.y)
+        utils.click()
+
+
+    def upgrade(self, path):
+        if [CONST_UPG_TOP, CONST_UPG_MID, CONST_UPG_BOT].count(path) == 0:
+            log.error("Tried to upgrade using invalid hotkey")
+        else:
+            self.select()
+            utils.press(hotkeys[path])
+            utils.press("esc")
+    
+
+    def set_targeting(self, desired_targeting):
+        if self.targets.count(desired_targeting) == 0:
+            log.error("Tried to set targeting using invalid hotkey")
+        self.select()
+        if self.type == "spac":
+            return
+        while self.target != self.targets.index(desired_targeting):
+            utils.press(hotkeys["targ_next"])
+            self.target = (self.target + 1) % 4
+        utils.press("esc")
+
+
+    def has_left_menu(self):
+        self.x >= 835
+
+
+    def print_type(self):
+        print("I am a " + self.type)
+
+
+def start_game():
+    utils.press(hotkeys["play_ff"])
+    sleep(0.5)
+    utils.press(hotkeys["play_ff"])
+
+
+# Upgrade monkey located at given position on given path
+def upgrade(path, position):
+    if [CONST_UPG_TOP, CONST_UPG_MID, CONST_UPG_BOT].count(path) == 0:
+        raise ValueError("Tried to upgrade using invalid hotkey")
+    else:
+        x = position[0]
+        y = position[1]
+        utils.move_cursor(x, y)
+        utils.click()
+        utils.press(hotkeys[path])
+        utils.press("esc")
+
+
+# returns current cash or -1 if OCR fails (deprecated in favour of a non-ML approach)
+"""
+def find_cash():
+    utils.take_screenshot()
+    screenshot = cv.imread("screenshots/monitor1.png")
+    # screenshot = cv.medianBlur(screenshot, 5)
+    gray = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
+    thresh = cv.threshold(gray, 220, 255, cv.THRESH_BINARY_INV)[1]
+    #,+ cv.THRESH_OTSU)[1]
+    cash_crop = thresh[20:65, 345:500]
+    kernel = np.ones((3,3),np.uint8)
+    cash_crop = cv.erode(cash_crop ,kernel,iterations = 2)
+    cv.imwrite('cash.png', cash_crop)
+
+    custom_oem_psm_config = r'--oem 3 --psm 13'
+    tesseract_out = pytesseract.image_to_string(cash_crop,
+                                                config=custom_oem_psm_config)  # Tesseract does its best to recognise something
+    found_cash_list = [s for s in list(tesseract_out) if s.isdigit()]  # Find digits in what was recognised
+    print(tesseract_out)
+    try:
+        found_cash = int("".join(found_cash_list))
+        return found_cash
+    except ValueError:
+        print("Could not recognise cash value")
+        return -1
+"""
+
+
+# returns current cash or -1 if template matching fails
+def find_cash():
+    utils.take_screenshot()
+    screenshot = cv.imread("screenshots/monitor1.png", cv.IMREAD_GRAYSCALE)
+    thresh = cv.adaptiveThreshold(screenshot, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 7, 2)
+    cash_crop = thresh[20:65, 345:530]
+    digits = {}
+    for i in range(10):
+        digit = cv.imread("numbers/" + str(i) + ".png", cv.IMREAD_GRAYSCALE)
+        w, h = digit.shape[::-1]                                      # Dimensions of input template, only 2 arguments because image is grayscale
+        match = cv.matchTemplate(cash_crop, digit, cv.TM_CCOEFF_NORMED)
+        threshold = 0.75
+        loc = np.where(match >= threshold)                            # NumPy is a fuck
+        last_x = False
+        loc[1].sort()
+        true_loc = loc[1].tolist()                                    # Have to convert NumPy array to a normal fucking list
+        for x in loc[1]:                                              # Goes through list of x-coordinates of matched points
+            if isinstance(last_x, np.int64) and x - last_x < w // 2:  # Checks if last_x is a NumPy int64 and if it's too close to the next element
+                true_loc.pop(true_loc.index(last_x))                  # Removes last_x from list if it's too close to the next element
+            last_x = x                                                # Sets last_x up for next iteration and we go agane
+        digits.update(dict.fromkeys(true_loc, i))
+    sorted_digits = sorted(digits.items())
+    sorted_digits = [str(x[1]) for x in sorted_digits]
+    try:
+        found_cash = int("".join(sorted_digits))
+        # cv.imwrite("debug/" + str(found_cash) + ".png", cash_crop)
+        return found_cash
+    except ValueError:
+        print("Could not recognise cash value")
+        return -1
+
+
+def wait_for_cash(amount):
+    sleep(1)
+    current_cash = find_cash()
+    while current_cash < amount:
+        print("current: " + str(current_cash) + " < " + str(amount))
+        sleep(3)
+        current_cash = find_cash()
+    else:
+        print(str(current_cash) + " > " + str(amount))
+        return
+
+
+# returns current round or -1 if OCR fails (deprecated in favour of a non-ML approach)
+"""
+def find_round():
+    utils.take_screenshot()
+    screenshot = cv.imread("screenshots/monitor1.png")
+    gray = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
+    thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+    round_crop = thresh[30:65, 1430:1485]  # crop
+    cv.imwrite('round.png', round_crop)
+    custom_oem_psm_config = r'--oem 3 --psm 13'
+    tesseract_out = pytesseract.image_to_string(round_crop,
+                                                config=custom_oem_psm_config)  # Tesseract does its best to recognise something
+    print(tesseract_out)
+    found_round_list = [s for s in list(tesseract_out) if s.isdigit()]  # Find digits in what was recognised
+    try:
+        found_round = int("".join(found_round_list))
+        return found_round
+    except ValueError:
+        print("Could not recognise round value")
+        return -1
+"""
+
+
+# returns current round or -1 if template matching fails
+def find_round():
+    utils.take_screenshot()
+    screenshot = cv.imread("screenshots/monitor1.png", cv.IMREAD_GRAYSCALE)
+    thresh = cv.adaptiveThreshold(screenshot, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 7, 2)
+    round_crop = thresh[28:70, 1420:1486]
+    digits = {}
+    for i in range(10):
+        digit = cv.imread("numbers/" + str(i) + ".png", cv.IMREAD_GRAYSCALE)
+        w, h = digit.shape[::-1]                                      # Dimensions of input template, only 2 arguments because image is grayscale
+        match = cv.matchTemplate(round_crop, digit, cv.TM_CCOEFF_NORMED)
+        threshold = 0.75
+        loc = np.where(match >= threshold)                            # NumPy is a fuck
+        last_x = False
+        loc[1].sort()
+        true_loc = loc[1].tolist()                                    # Have to convert NumPy array to a normal fucking list
+        for x in loc[1]:                                              # Goes through list of x-coordinates of matched points
+            if isinstance(last_x, np.int64) and x - last_x < w // 2:  # Checks if last_x is a NumPy int64 and if it's too close to the next element
+                true_loc.pop(true_loc.index(last_x))                  # Removes last_x from list if it's too close to the next element
+            last_x = x                                                # Sets last_x up for next iteration and we go agane
+        digits.update(dict.fromkeys(true_loc, i))
+    sorted_digits = sorted(digits.items())
+    sorted_digits = [str(x[1]) for x in sorted_digits]
+    try:
+        found_round = int("".join(sorted_digits))
+        return found_round
+    except ValueError:
+        print("Could not recognise round value")
+        return -1
+
+
+
+def wait_for_round(number):
+    sleep(1)
+    current_round = find_round()
+    while current_round < number:
+        print("current round: " + str(current_round) + " < " + str(number))
+        sleep(3)
+        current_round = find_round()
+    else:
+        print("current round: " + str(current_round) + " >= " + str(number))
+        return
+
+
+def wait_for_victory(seconds):
+    sleep(seconds)
+
+
+def place_monkey(monkey, x, y):
+    utils.move_cursor(x, y)
+    desired_key = hotkeys[monkey]
+    utils.press(desired_key)
+    utils.click()
+    placed_monkeys.update({monkey: (x, y)})
+
+
+# Sets targeting for tower located at position to Strong for normal towers or Smart for spactory
+def set_targeting(position):
+    x = position[0]
+    y = position[1]
+    utils.move_cursor(x, y)
+    utils.click()
+    utils.press(hotkeys["targ_prev"])
+    utils.press("escape")
+
+
+def solve(map):
+    eval("solve_" + map + "()")
+
+
+def solve_sanctuary():
+    wait_for_cash(920)
+    Monkey("hero", 833, 150)
+    wait_for_cash(185)
+    Monkey("sniper", 256, 964)
+    utils.click()
+    utils.press(hotkeys["targ_prev"])
+    wait_for_cash(340)
+    utils.press(hotkeys[CONST_UPG_BOT])
+    wait_for_cash(340)
+    utils.press(hotkeys[CONST_UPG_BOT])
+    wait_for_cash(295)
+    utils.press(hotkeys[CONST_UPG_TOP])
+    wait_for_cash(2975)
+    utils.press(hotkeys[CONST_UPG_BOT])
+    wait_for_cash(3610)
+    utils.press(hotkeys[CONST_UPG_BOT])
+    wait_for_cash(1275)
+    utils.press(hotkeys[CONST_UPG_TOP])
+    wait_for_cash(11900)
+    utils.press(hotkeys[CONST_UPG_BOT])
+    utils.press("escape")
+    wait_for_round(39)
+    wait_for_victory(20)
+
+
+def solve_ravine():
+    dart1 = Monkey("dart", 1353, 266)
+    wait_for_cash(920)
+    Monkey("hero", 703, 109)
+    wait_for_cash(420)
+    ace1 = Monkey("ace", 534, 829)
+    wait_for_cash(425)
+    ace1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(255)
+    ace1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(1870)
+    ace1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(470)
+    alch1 = Monkey("alch", 359, 814)
+    wait_for_cash(505)
+    alch1.upgrade(CONST_UPG_TOP)
+    alch1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(1060)
+    alch1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(615)
+    alch1.upgrade(CONST_UPG_MID)
+    alch1.upgrade(CONST_UPG_MID)
+    wait_for_cash(1100)
+    ace1.upgrade(CONST_UPG_TOP)
+    ace1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(2550)
+    alch1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(1600)
+    sniper1 = Monkey("sniper", 123, 887)
+    sniper1.upgrade(CONST_UPG_MID)
+    sniper1.upgrade(CONST_UPG_MID)
+    sniper1.upgrade(CONST_UPG_BOT)
+    sniper1.upgrade(CONST_UPG_BOT)
+    wait_for_round(39)
+    wait_for_victory(20)
+
+
+def solve_flooded_valley():
+    wait_for_cash(920)
+    Monkey("hero", 745, 429)
+    wait_for_cash(170)
+    sub1 = Monkey("sub", 973, 197)
+    wait_for_cash(380)
+    sub1.upgrade(CONST_UPG_MID)
+    wait_for_cash(380)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(850)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(935)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(255)
+    sub1.upgrade(CONST_UPG_MID)
+    wait_for_cash(260)
+    sub2 = Monkey("sub", 1011, 841)
+    wait_for_cash(535)
+    sub2.upgrade(CONST_UPG_TOP)
+    sub2.upgrade(CONST_UPG_TOP)
+    wait_for_cash(1230)
+    sub2.upgrade(CONST_UPG_BOT)
+    sub2.upgrade(CONST_UPG_BOT)
+    wait_for_cash(3485)
+    sub2.upgrade(CONST_UPG_BOT)
+    sub2.upgrade(CONST_UPG_BOT)
+    wait_for_cash(2550)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_round(39)
+    wait_for_victory(20)
+
+
+def solve_infernal():
+    dart1 = Monkey("dart", 834, 387)
+    wait_for_cash(920)
+    Monkey("hero", 122, 643)
+    wait_for_cash(75)
+    dart1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(170)
+    sub1 = Monkey("sub", 487, 789)
+    wait_for_cash(535)
+    sub1.upgrade(CONST_UPG_TOP)
+    sub1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(170)
+    dart1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(1230)
+    sub1.upgrade(CONST_UPG_BOT)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(285)
+    sniper1 = Monkey("sniper", 1568, 599)
+    sniper1.set_targeting("strong")
+    wait_for_cash(975)
+    sniper1.upgrade(CONST_UPG_TOP)
+    sniper1.upgrade(CONST_UPG_BOT)
+    sniper1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(935)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(2550)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_round(39)
+    wait_for_victory(20)
+
+
+def solve_bloody_puddles():
+    dart1 = Monkey("dart", 393, 304)
+    wait_for_cash(920)
+    Monkey("hero", 836, 434)
+    wait_for_cash(170)
+    sub1 = Monkey("sub", 1217, 175)
+    wait_for_cash(535)
+    sub1.upgrade(CONST_UPG_TOP)
+    sub1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(170)
+    dart2 = Monkey("dart", 939, 354)
+    wait_for_cash(170)
+    dart3 = Monkey("dart", 1255, 833)
+    wait_for_cash(380)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(850)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(1260)
+    sniper1 = Monkey("sniper", 712, 62)
+    sniper1.set_targeting("strong")
+    sniper1.upgrade(CONST_UPG_TOP)
+    sniper1.upgrade(CONST_UPG_BOT)
+    sniper1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(735)
+    dart1.upgrade(CONST_UPG_BOT)
+    dart1.upgrade(CONST_UPG_BOT)
+    dart2.upgrade(CONST_UPG_BOT)
+    dart2.upgrade(CONST_UPG_BOT)
+    dart3.upgrade(CONST_UPG_BOT)
+    dart3.upgrade(CONST_UPG_BOT)
+    wait_for_cash(415)
+    dart4 = Monkey("dart", 357, 806)
+    dart4.upgrade(CONST_UPG_BOT)
+    dart4.upgrade(CONST_UPG_BOT)
+    wait_for_cash(935)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(2550)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_round(39)
+    wait_for_victory(20)
+
+
+def solve_workshop():
+    dart1 = Monkey("dart", 920, 502)
+    wait_for_cash(920)
+    Monkey("hero", 1214, 267)
+    wait_for_cash(340)
+    wiz1 = Monkey("wizard", 1020, 499)
+    wait_for_cash(255)
+    wiz1.upgrade(CONST_UPG_MID)
+    wait_for_cash(765)
+    wiz1.upgrade(CONST_UPG_MID)
+    wait_for_cash(230)
+    wiz1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(255)
+    wiz1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(700)
+    spac1 = Monkey("spac", 1016, 409)
+    wait_for_cash(680)
+    spac1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(465)
+    spac1.upgrade(CONST_UPG_BOT)
+    spac1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(1190)
+    spac1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(2975)
+    spac1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(510)
+    spac1.upgrade(CONST_UPG_TOP)
+    wait_for_round(39)
+    wait_for_victory(20)
+
+
+def solve_quad():
+    dart1 = Monkey("dart", 834, 270)
+    wait_for_cash(920)
+    Monkey("hero", 1154, 322)
+    wait_for_cash(170)
+    sub1 = Monkey("sub", 960, 623)
+    wait_for_cash(340)
+    wiz1 = Monkey("wizard", 1267, 598)
+    wait_for_cash(1020)
+    wiz1.upgrade(CONST_UPG_MID)
+    wiz1.upgrade(CONST_UPG_MID)
+    wait_for_cash(230)
+    wiz1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(255)
+    wiz1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(700)
+    spac1 = Monkey("spac", 398, 525)
+    wait_for_cash(680)
+    spac1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(510)
+    spac1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(580)
+    sniper1 = Monkey("sniper", 840, 714)
+    sniper1.set_targeting("strong")
+    sniper1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(680)
+    sniper1.upgrade(CONST_UPG_BOT)
+    sniper1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(1275)
+    sniper1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(2975)
+    sniper1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(3610)
+    sniper1.upgrade(CONST_UPG_BOT)
+    wait_for_round(39)
+    wait_for_victory(20)
+
+
+def solve_dark_castle():
+    place_monkey("dart", 577, 491)
+    wait_for_cash(920)
+    place_monkey("hero", 910, 144)
+    wait_for_cash(170)
+    place_monkey("sub", 1083, 694)
+    wait_for_cash(380)
+    upgrade(CONST_UPG_BOT, placed_monkeys["sub"])
+    wait_for_cash(850)
+    upgrade(CONST_UPG_BOT, placed_monkeys["sub"])
+    wait_for_cash(535)
+    upgrade(CONST_UPG_TOP, placed_monkeys["sub"])
+    upgrade(CONST_UPG_TOP, placed_monkeys["sub"])
+    wait_for_cash(245)
+    upgrade(CONST_UPG_BOT, placed_monkeys["dart"])
+    upgrade(CONST_UPG_BOT, placed_monkeys["dart"])
+    wait_for_cash(935)
+    upgrade(CONST_UPG_BOT, placed_monkeys["sub"])
+    wait_for_cash(2550)
+    upgrade(CONST_UPG_BOT, placed_monkeys["sub"])
+    wait_for_cash(530)
+    upgrade(CONST_UPG_BOT, placed_monkeys["dart"])
+    wait_for_cash(470)
+    place_monkey("alch", 924, 666)
+    wait_for_cash(1700)
+    upgrade(CONST_UPG_TOP, placed_monkeys["alch"])
+    upgrade(CONST_UPG_TOP, placed_monkeys["alch"])
+    upgrade(CONST_UPG_TOP, placed_monkeys["alch"])
+    wait_for_cash(615)
+    upgrade(CONST_UPG_MID, placed_monkeys["alch"])
+    upgrade(CONST_UPG_MID, placed_monkeys["alch"])
+    wait_for_cash(2550)
+    upgrade(CONST_UPG_TOP, placed_monkeys["alch"])
+
+    # just in case
+    wait_for_cash(6000)
+    upgrade(CONST_UPG_BOT, placed_monkeys["sub"])
+    upgrade(CONST_UPG_TOP, placed_monkeys["alch"])
+    wait_for_round(39)
+    wait_for_victory(20)
+
+
+def solve_muddy_puddles():
+    dart1 = Monkey("dart", 467, 296)
+    wait_for_cash(920)
+    Monkey("hero", 980, 690)
+    wait_for_cash(170)
+    sub1 = Monkey("sub", 1203, 450)
+    wait_for_cash(535)
+    sub1.upgrade(CONST_UPG_TOP)
+    sub1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(380)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(850)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(285)
+    sniper1 = Monkey("sniper", 1229, 43)
+    sniper1.set_targeting("strong")
+    wait_for_cash(975)
+    sniper1.upgrade(CONST_UPG_TOP)
+    sniper1.upgrade(CONST_UPG_BOT)
+    sniper1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(660)
+    dart2 = Monkey("dart", 1138, 775)
+    dart1.upgrade(CONST_UPG_BOT)
+    dart1.upgrade(CONST_UPG_BOT)
+    dart2.upgrade(CONST_UPG_BOT)
+    dart2.upgrade(CONST_UPG_BOT)
+    wait_for_cash(935)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(2550)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_round(39)
+    wait_for_victory(20)
+
+
+def solve_ouch():
+    dart1 = Monkey("dart", 666, 507)
+    wait_for_cash(920)
+    Monkey("hero", 1133, 322)
+    wait_for_cash(170)
+    sub1 = Monkey("sub", 978, 612)
+    wait_for_cash(535)
+    sub1.upgrade(CONST_UPG_TOP)
+    sub1.upgrade(CONST_UPG_TOP)
+    wait_for_cash(380)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(850)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(245)
+    dart1.upgrade(CONST_UPG_BOT)
+    dart1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(530)
+    dart1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(245)
+    dart1.upgrade(CONST_UPG_MID)
+    dart1.upgrade(CONST_UPG_MID)
+    wait_for_cash(285)
+    sniper1 = Monkey("sniper", 1023, 324)
+    sniper1.set_targeting("strong")
+    wait_for_cash(975)
+    sniper1.upgrade(CONST_UPG_TOP)
+    sniper1.upgrade(CONST_UPG_BOT)
+    sniper1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(935)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_cash(2550)
+    sub1.upgrade(CONST_UPG_BOT)
+    wait_for_round(39)
+    wait_for_victory(20)
